67a736b935e497d1f73e74e96e4c9f88ab6a3c69
frank boakye yiadom <fbyiadom@gmail.com>
2019-01-02 20:14:09 -0500

    add the blog and job apps

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
